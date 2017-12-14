ed8326abe7347714068bf8a86183f700d18297ce
root <root@nano.openair4G.eur>
2017-12-14 19:20:29 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickets::Application.config.secret_key_base = 'a9634e48223708eae722c7089afc23f51ae97bbf0d0f1cbefe05d199735b996dd243f2ddc90d83aabbd70ae1d2112afb84204c8b09447310e549b71ebca7094d'
